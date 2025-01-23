d87da64091e32c1fdcb6ea71c460a921bb81f082
Jinhee-Bae <jinhee.bae023@gmail.com>
2025-01-23 11:07:00 +0100

    List with Js

--- shopping-list/action.js
+++ shopping-list/action.js
@@ -1,12 +1,46 @@
-const product = document.querySelector('.input'); // input field
-const add = document.querySelector('.button');
-const item = document.getElementById('item'); // item of shopping list
-const trash = document.getElementById('trash');// trash icon
+document.addEventListener('DOMContentLoaded', () => {
+  const product = document.querySelector('#itemInput'); // input field
+  const add = document.querySelector('#addItemButton');
+  const itemList = document.querySelector('.items'); // item of shopping list
 
+  const addItem = () => {
+    const newItemText = product.value; // get input value
+    console.log(`Input value: ${newItemText}`);
 
+    if (newItemText) {
+      // create new item
+      const newItem = document.createElement('li');
+      newItem.classList.add('item');
+      newItem.innerHTML = `
+      <p>${newItemText}</p>
+      <p class="trash" style="cursor: pointer;">🗑️</p>
+    `;
 
-product.addEventListener("input", updateValue);
-function updateValue(e) {
-  item.innerHTML = product.value;
-  item.style.display = 'flex';
-}
+      // delete Item
+      newItem.querySelector('.trash').addEventListener('click', () => {
+        console.log('Trash clicked');
+        newItem.remove();
+      });
+
+      // add item on list
+      itemList.appendChild(newItem);
+      console.log('New item added to list');
+      product.value = ''; // clear input field
+    }
+  };
+
+  product.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      addItem();
+    }
+  });
+
+  add.addEventListener('click', addItem);
+});
+
+// product.addEventListener("enter", updateValue);
+// function updateValue(e) {
+//   if(item && product)
+//   item.innerHTML = product.value;
+//   item.style.display = 'flex';
+// }
