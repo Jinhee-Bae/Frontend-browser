91ab58fbff1274fc34f2b422c66651f30088076e
jinhee bae <jinhee.bae023@gmail.com>
2025-02-19 12:57:06 +0100

    final code with event delegation

--- shopping-list copy/main.js
+++ shopping-list copy/main.js
@@ -1,9 +1,15 @@
 const addBtn = document.querySelector('.footer__button');
 const input = document.querySelector('.footer__input');
+const form = document.querySelector('.new-form');
 const items = document.querySelector('.items');
 
 /* consider the following:
 as a object orient Programming language, make a good use of nasting. put createItem function inside of onAdd() function. therefore createItem() function can only used through onAdd() funtion. */
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  onAdd();
+});
 
 function onAdd() {
   // 1. take the input value from the user
@@ -27,42 +33,29 @@ function onAdd() {
   input.focus();
 }
 
+let id = 0; //UUID
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
-
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
-
-  const itemName = document.createElement('span');
-  itemName.setAttribute('class', 'item__name');
-  itemName.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-  deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
-  item.appendChild(itemName);
-  item.appendChild(deleteBtn);
+  itemRow.setAttribute('data-id', id);
+  itemRow.innerHTML = `
+    <div class="item">
+      <span class="item__name">${text}</span>
+      <button class="item__delete">
+        <i class="fas fa-trash-alt" data-id=${id}></i>
+      </button>
+    </div>
+    <div class="item__divider"></div>
+`;
+  id++;
   return itemRow;
 }
 
-addBtn.addEventListener('click', () => {
-  console.log('add button clicked');
-  onAdd();
-});
-
-input.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    onAdd();
+items.addEventListener('click', (event) => {
+  event.preventDefault();
+  const id = event.target.dataset.id;
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
   }
 });
